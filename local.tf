ed650410f2eee51bfac81d472e134ed84385c74c
A PURUSHOTHAM REDDY <reddypurushotham1020@gmail.com>
2021-02-04 17:27:28 +0530

    remove file

--- local.tf
+++
@@ -1,8 +0,0 @@
-locals {
-    azs = length(data.aws_availability_zones.available.names)
-    azs_names = data.aws_availability_zones.available.names
-}
-
-data "aws_availability_zones" "available" {
-  state = "available"
-}
